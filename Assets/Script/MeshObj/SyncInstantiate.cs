1cc68c524a741f76c449d6af1db3e3a79a9be06d
ng-tk <naganuma.takeru@gmail.com>
2021-10-12 14:47:47 +0900

    [add]積み上げるオブジェクトを予めシーンに置くための仮置用のPrefabとそれを置き換えるスクリプトを追加
    仮置用のPrefabはAssets/Resources/TemporaryObjectsに入っており、TemporaryObjectというタグを設定してあります。
    置き換え用のスクリプトはAssets/Script/MeshObj/SyncInstantiate.csです。
    現状Hotdog以外のオブジェクトについては元のプレハブが正しく設定されていので生成に失敗しています。

---
+++ Assets/Script/MeshObj/SyncInstantiate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonobitEngine;
+
+//-----------------------------------------------------------------------------
+//! [制作者]     長沼豪琉
+//! [最終更新日] 2021/10/12
+//! [内容]       オブジェクトの同期生成
+//-----------------------------------------------------------------------------
+public class SyncInstantiate : MonobitEngine.MonoBehaviour
+{
+    [Header("同期生成するプレハブ")] public GameObject prefab; // 生成するプレハブ
+
+    // Update is called once per frame
+    void Update()
+    {
+        // ルームに入室しているか確認
+        if (MonobitNetwork.inRoom) {
+            // インスタンス生成
+            if (prefab) {
+                var obj = MonobitNetwork.Instantiate(prefab.name, this.transform.position, this.transform.rotation, 0);
+                // 生成できたか確認
+                if (!obj) {
+                    Debug.LogWarning(this.name + "はオブジェクトの生成に失敗しました。");
+                }
+            }
+            else {
+                Debug.LogWarning(this.name + "はプレハブが設定されていません。");
+            }
+            // 自身を削除
+            Destroy(this.gameObject);
+        }
+    }
+}
