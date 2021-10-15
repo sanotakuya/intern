e046c1a2868c5a3269f85cf9fda2bdf947982823
ng-tk <naganuma.takeru@gmail.com>
2021-10-15 16:26:10 +0900

    [update]残り時間の表示機能を追加

--- Assets/naganuma/Script/InGameUIManager.cs
+++ Assets/naganuma/Script/InGameUIManager.cs
@@ -22,10 +22,10 @@ public class InGameUIManager : MonoBehaviour
     //! Inspectorに公開する変数
     //-----------------------------------------------------------------------------
     [Header("RegisterScore")]  public RegisterScore registerScore; // スコア
-    //[Header("GameTimer")]      public GameTimer     gameTimer    ;
-    [Header("単位テキスト")]   public Text UnitText              ; // 単位テキスト
-    [Header("スコアテキスト")] public Text scoreText             ; // スコアテキスト
-    [Header("タイムテキスト")] public Text timeText              ; // タイムテキスト
+    [Header("GameTimer")]      public GameTimer     gameTimer    ; // ゲームタイマー
+    [Header("単位テキスト")]   public Text          UnitText     ; // 単位テキスト
+    [Header("スコアテキスト")] public Text          scoreText    ; // スコアテキスト
+    [Header("タイムテキスト")] public Text          timeText     ; // タイムテキスト
 
     //-----------------------------------------------------------------------------
     //! [内容]    開始処理
@@ -33,7 +33,8 @@ public class InGameUIManager : MonoBehaviour
     void Start()
     {
         // インスペクターで指定されているかチェック
-        if (!registerScore) Debug.LogError("RegisterScoreが指定されていません。");
+        //if (!registerScore) Debug.LogError("RegisterScoreが指定されていません。");
+        if (!gameTimer)     Debug.LogError("ゲームタイマーが指定されていません。");
         if (!UnitText)      Debug.LogError("単位テキストが指定されていません。");
         if (!scoreText)     Debug.LogError("スコアテキストが指定されていません。");
         if (!timeText)      Debug.LogError("タイムテキストが指定されていません。");
@@ -44,6 +45,9 @@ public class InGameUIManager : MonoBehaviour
     //-----------------------------------------------------------------------------
     void Update()
     {
-
+        // 残り時間から分と秒を計算
+        int minutes = Mathf.FloorToInt(gameTimer.limitCount / 60.0f)          ; // 分
+        int seconds = Mathf.FloorToInt(gameTimer.limitCount - minutes * 60.0f); // 秒
+        timeText.text = string.Format("{0:00}:{0:00}", minutes, seconds);
     }
 }
