24cc4ec6966c8f87c0942ba5039afd7cf32d990d
ryu-asd <ryu1228sq@gmail.com>
2021-10-15 11:39:35 +0900

    [add]RealTimeTextManagerの追加

---
+++ Assets/Script/UI/RealTimeText/RealTimeTextManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//-----------------------------------------------------------------------------
+//! [制作者]		小野龍大
+//!	[最終更新日]	2021/10/15
+//! [内容]		リアルタイムテキストの管理
+//-----------------------------------------------------------------------------
+public class RealTimeTextManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
