6351a7ca7ae26b02cbf4f87da792917973f76d5a
ng-tk <naganuma.takeru@gmail.com>
2021-10-18 12:34:02 +0900

    [add]所有権を管理するスクリプトを追加

---
+++ Assets/Script/MeshObj/OwnershipManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonobitEngine;
+
+//-----------------------------------------------------------------------------
+//! [制作者]     長沼豪琉
+//! [最終更新日] 2021/10/18
+//! [内容]       所有権の管理
+//-----------------------------------------------------------------------------
+public class OwnershipManager : MonobitEngine.MonoBehaviour
+{
+    //-----------------------------------------------------------------------------
+    //! private変数
+    //-----------------------------------------------------------------------------
+    private List<MonobitView> manageMonobitViews = new List<MonobitView>(); // MonobitViewリスト
+    private MonobitPlayer     currentHost                                 ; // 現在のホスト
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    RPC受信関数(現在のスコア)
+    //-----------------------------------------------------------------------------
+    [MunRPC]
+    void SendOwner(MonobitPlayer monobitPlayer)
+    {
+        currentHost = monobitPlayer;
+    }
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    更新処理
+    //-----------------------------------------------------------------------------
+    void Update()
+    {
+        // 自分がホストな場合
+        if (MonobitNetwork.isHost) {
+            var player = MonobitNetwork.player;
+            // 現在の所有者が自分と違う場合
+            if (player != currentHost) {
+                // 自分を所有者として送信
+                SendOwner(player);
+                // 所有者を変更する
+                foreach (var monobitView in manageMonobitViews) {
+                    monobitView.TransferOwnership(currentHost);
+                }
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    所有権管理に追加(管理に追加されたオブジェクトは所有者が消えても削除されなくなります。)
+    //! [引数]    管理に追加するオブジェクトのMonobitView
+    //-----------------------------------------------------------------------------
+    public void AddManage(MonobitView monobitView)
+    {
+        // 所有者が消えてもオブジェクトが残るように
+        monobitView.isDontDestroyOnRoom = true;
+        // 管理リストに追加
+        manageMonobitViews.Add(monobitView);
+    }
+}
--- Assets/Script/MeshObj/SyncInstantiate.cs
+++ Assets/Script/MeshObj/SyncInstantiate.cs
@@ -5,7 +5,7 @@ using MonobitEngine;
 
 //-----------------------------------------------------------------------------
 //! [制作者]     長沼豪琉
-//! [最終更新日] 2021/10/15
+//! [最終更新日] 2021/10/18
 //! [内容]       オブジェクトの同期生成
 //-----------------------------------------------------------------------------
 public class SyncInstantiate : MonobitEngine.MonoBehaviour
@@ -13,37 +13,52 @@ public class SyncInstantiate : MonobitEngine.MonoBehaviour
     //-----------------------------------------------------------------------------
     //! public変数
     //-----------------------------------------------------------------------------
-    [Header("プレハブが存在するパス")] public string     path  ; // プレハブのパス
-    [Header("同期生成するプレハブ")]   public GameObject prefab; // 生成するプレハブ
+    [Header("OwnershipManager")]       public OwnershipManager ownershipManager; // 所有権管理
+    [Header("プレハブが存在するパス")] public string           path            ; // プレハブのパス
+    [Header("同期生成するプレハブ")]   public GameObject       prefab          ; // 生成するプレハブ
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    開始処理
+    //-----------------------------------------------------------------------------
+    void Start()
+    {
+        if (!prefab) {
+            Debug.LogError(this.name + "でプレハブが設定されていません。");
+        }
+        if (!ownershipManager) {
+            Debug.LogError(this.name + "でOwnershipManagerが設定されていません。");
+        }
+    }
 
     //-----------------------------------------------------------------------------
     //! [内容]    更新処理
     //-----------------------------------------------------------------------------
     void Update()
     {
-            // ルームに入室しているか確認
-            if (MonobitNetwork.inRoom)
+        // ルームに入室しているか確認
+        if (MonobitNetwork.inRoom)
+        {
+            // 自身がルームのホストの場合はインスタンス化
+            if (MonobitNetwork.isHost)
             {
-                // 自身がルームのホストの場合はインスタンス化
-                if (MonobitNetwork.isHost)
-                {
-                    // インスタンス生成
-                    if (prefab)
-                    {
-                        var obj = MonobitNetwork.Instantiate(path + prefab.name, this.transform.position, this.transform.rotation, 0);
-                        // 生成できたか確認
-                        if (!obj)
-                        {
-                            Debug.LogWarning(this.name + "はオブジェクトの生成に失敗しました。");
-                        }
+                // インスタンス生成
+                var obj = MonobitNetwork.Instantiate(path + prefab.name, this.transform.position, this.transform.rotation, 0);
+                // 生成できたか確認
+                if (obj) {
+                    var monobitView = obj.GetComponent<MonobitView>();
+                    if (monobitView) {
+                        ownershipManager.AddManage(monobitView);
                     }
-                    else
-                    {
-                        Debug.LogWarning(this.name + "はプレハブが設定されていません。");
+                    else {
+                        Debug.LogWarning(this.name + "はMonobitViewがアタッチされていません。");
                     }
                 }
-                // 自身を削除
-                Destroy(this.gameObject);
+                else {
+                    Debug.LogWarning(this.name + "はオブジェクトの生成に失敗しました。");
+                }
             }
+            // 自身を削除
+            Destroy(this.gameObject);
+        }
     }
 }
