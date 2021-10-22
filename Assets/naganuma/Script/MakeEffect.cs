1f983c4fdbd7ca93976bf5ef8463fd554f047374
ng-tk <naganuma.takeru@gmail.com>
2021-10-22 11:57:03 +0900

    [add]エフェクトを生成するスクリプトを作成、崩れた時のエフェクトを追加

---
+++ Assets/naganuma/Script/MakeEffect.cs
@@ -0,0 +1,98 @@
+﻿//#define OFFLINE
+
+using   System.Collections;
+using   System.Collections.Generic;
+using   UnityEngine;
+using   MonobitEngine;
+
+//-----------------------------------------------------------------------------
+//! [制作者]     長沼豪琉
+//! [最終更新日] 2021/10/22
+//! [内容]       エフェクト発生
+//-----------------------------------------------------------------------------
+public class MakeEffect : MonobitEngine.MonoBehaviour
+{
+    //-----------------------------------------------------------------------------
+    //! private変数
+    //-----------------------------------------------------------------------------
+    private Rigidbody rigidbody;
+
+    //-----------------------------------------------------------------------------
+    //! public変数
+    //-----------------------------------------------------------------------------
+
+    //-----------------------------------------------------------------------------
+    //! Inspectorに公開する変数
+    //-----------------------------------------------------------------------------
+    [Header("パーティクルサイズ")]                             public float particleSize;
+    [Header("パーティクルを発生させるオブジェクトのスピード")] public float objectSpeed;
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    更新処理
+    //-----------------------------------------------------------------------------
+    void Update()
+    {
+        // 自分がホストじゃない場合は処理を行わない
+        if (MonobitNetwork.isHost) {
+            // リジッドボディを検索
+            if (!rigidbody) {
+                rigidbody = this.GetComponent<Rigidbody>();
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    当たり判定
+    //-----------------------------------------------------------------------------
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (rigidbody) {
+            // スピードが指定数以上の場合は処理
+            if (rigidbody.velocity.magnitude >= objectSpeed) {
+#if !OFFLINE
+                var gameObject = MonobitNetwork.Instantiate("Effect/Smoke", this.transform.position, Quaternion.identity, 0);
+#else
+                var prefab     = Resources.Load("Effect/Smoke") as GameObject;
+                var gameObject = Instantiate(prefab, this.transform.position, Quaternion.identity);
+#endif
+                if (gameObject) {
+                    gameObject.transform.localScale = new Vector3(particleSize, particleSize, particleSize);
+                }
+            }
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    カートに積み上げた時のエフェクトを再生
+    //-----------------------------------------------------------------------------
+    public void PlayAddEffect()
+    {
+#if !OFFLINE
+        var gameObject = MonobitNetwork.Instantiate("Effect/NumAddDisplay", this.transform.position, Quaternion.identity, 0);
+#else
+        var prefab     = Resources.Load("Effect/NumAddDisplay") as GameObject;
+        var gameObject = Instantiate(prefab, this.transform.position, Quaternion.identity);
+#endif
+        if (gameObject) {
+            var scoreDisplay = gameObject.GetComponentInChildren<ScoreDisplay>();
+            scoreDisplay.target = this.transform;
+        }
+    }
+
+    //-----------------------------------------------------------------------------
+    //! [内容]    カートから離れた時のエフェクトを再生
+    //-----------------------------------------------------------------------------
+    public void PlaySubEffect()
+    {
+#if !OFFLINE
+        var gameObject = MonobitNetwork.Instantiate("Effect/NumSubDisplay", this.transform.position, Quaternion.identity, 0);
+#else
+        var prefab     = Resources.Load("Effect/NumSubDisplay") as GameObject;
+        var gameObject = Instantiate(prefab, this.transform.position, Quaternion.identity);
+#endif
+        if (gameObject) {
+            var scoreDisplay = gameObject.GetComponentInChildren<ScoreDisplay>();
+            scoreDisplay.target = this.transform;
+        }
+    }
+}
