4dda7d4c790d3f43bd984c40e9b2b9edbb4258bc
ryu-asd <ryu1228sq@gmail.com>
2021-10-27 18:19:32 +0900

    [add]waitTextの追加

---
+++ Assets/StartText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonobitEngine;
+using TMPro;
+
+
+public class StartText : MonobitEngine.MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI target;
+    [SerializeField] GameObject gameUi;
+
+    private void Update()
+    {
+        if(MonobitNetwork.isHost)
+        {
+            target.text = "Enterでゲーム開始\n(注意)途中参加はできません";
+        }
+        else
+        {
+            target.text = "ホストのゲーム開始まで待機中";
+
+        }
+
+        if(gameUi.active)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
